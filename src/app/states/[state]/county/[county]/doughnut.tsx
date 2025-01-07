19b58a64b7716a28c6c4ab658c657610c4aefaee
ryankuah <ryzkuah@gmail.com>
2025-01-07 21:40:52 +1100

    cssstylesheet

--- src/app/states/[state]/county/[county]/doughnut.tsx
+++ src/app/states/[state]/county/[county]/doughnut.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export function LabourDoughnut({
   doughnutData,
