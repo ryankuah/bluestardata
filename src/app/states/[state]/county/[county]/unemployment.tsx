e65e04d8c63258de8fa3db4b9192ee388fdc9951
ryankuah <ryzkuah@gmail.com>
2025-01-07 22:35:14 +1100

    please please please

--- src/app/states/[state]/county/[county]/unemployment.tsx
+++ src/app/states/[state]/county/[county]/unemployment.tsx
@@ -20,32 +20,50 @@ export default async function Unemployment({
     stateName: state,
   };
 
-  for (let i = 2014; i <= 2023; i++) {
-    const tempState = (await fetch(
-      `https://api.census.gov/data/${i}/acs/acsse?get=NAME,K202301_002E,K202301_005E&for=state:${stateFips}`,
-    )
-      .then((res) => res.json())
-      .catch(() => console.log("State", i))) as result;
-    const tempCountry = (await fetch(
-      `https://api.census.gov/data/${i}/acs/acsse?get=NAME,K202301_002E,K202301_005E&for=us`,
-    )
-      .then((res) => res.json())
-      .catch(() => console.log("Country", i))) as result;
-    const tempCounty = (await fetch(
-      `https://api.census.gov/data/${i}/acs/acsse?get=NAME,K202301_002E,K202301_005E&for=county:${countyFips}&in=state:${stateFips}`,
-    )
-      .then((res) => res.json())
-      .catch(() => console.log("County", i))) as result;
-    unemployments.state.push(
-      (tempState?.[1]?.[2] ?? 0) / (tempState?.[1]?.[1] ?? 1) || null,
-    );
-    unemployments.country.push(
-      (tempCountry?.[1]?.[2] ?? 0) / (tempCountry?.[1]?.[1] ?? 1) || null,
-    );
-    unemployments.county.push(
-      (tempCounty?.[1]?.[2] ?? 0) / (tempCounty?.[1]?.[1] ?? 1) || null,
-    );
-    unemployments.label.push(i.toString());
-  }
+  await Promise.all(
+    Array.from({ length: 2023 - 2014 + 1 }, async (_, index) => {
+      const i = 2014 + index;
+
+      const [tempState, tempCountry, tempCounty] = (await Promise.all([
+        fetch(
+          `https://api.census.gov/data/${i}/acs/acsse?get=NAME,K202301_002E,K202301_005E&for=state:${stateFips}`,
+        )
+          .then((res) => res.json())
+          .catch(() => {
+            console.log("State", i);
+            return null;
+          }),
+
+        fetch(
+          `https://api.census.gov/data/${i}/acs/acsse?get=NAME,K202301_002E,K202301_005E&for=us`,
+        )
+          .then((res) => res.json())
+          .catch(() => {
+            console.log("Country", i);
+            return null;
+          }),
+
+        fetch(
+          `https://api.census.gov/data/${i}/acs/acsse?get=NAME,K202301_002E,K202301_005E&for=county:${countyFips}&in=state:${stateFips}`,
+        )
+          .then((res) => res.json())
+          .catch(() => {
+            console.log("County", i);
+            return null;
+          }),
+      ])) as [result, result, result];
+
+      unemployments.state.push(
+        (tempState?.[1]?.[2] ?? 0) / (tempState?.[1]?.[1] ?? 1) || null,
+      );
+      unemployments.country.push(
+        (tempCountry?.[1]?.[2] ?? 0) / (tempCountry?.[1]?.[1] ?? 1) || null,
+      );
+      unemployments.county.push(
+        (tempCounty?.[1]?.[2] ?? 0) / (tempCounty?.[1]?.[1] ?? 1) || null,
+      );
+      unemployments.label.push(i.toString());
+    }),
+  );
   return <Chart unemployments={unemployments} />;
 }
