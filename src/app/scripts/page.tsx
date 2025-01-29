af1b5f1469314bc248ddff8743e3248902c8dedc
ryankuah <ryzkuah@gmail.com>
2025-01-29 16:37:47 +1100

    Script

--- src/app/scripts/page.tsx
+++ src/app/scripts/page.tsx
@@ -1,12 +1,32 @@
-"use client";
 import { addStateData, addCountyData } from "@/app/scripts/scripts";
 import { addFredIds } from "@/utils/fred/utils";
-export default function Page() {
+export default async function Page() {
+  async function handleStateData() {
+    'use server'
+    await addStateData();
+  }
+
+  async function handleCountyData() {
+    'use server'
+    await addCountyData();
+  }
+
+  async function handleFredIds() {
+    'use server'
+    await addFredIds();
+  }
+
   return (
     <div className="m-4 flex flex-col">
-      <button onClick={async () => addStateData()}>Add State Data</button>
-      <button onClick={async () => addCountyData()}>Add County Data</button>
-      <button onClick={async () => addFredIds()}>Add Fred Ids</button>
+      <form action={handleStateData}>
+        <button type="submit">Add State Data</button>
+      </form>
+      <form action={handleCountyData}>
+        <button type="submit">Add County Data</button>
+      </form>
+      <form action={handleFredIds}>
+        <button type="submit">Add Fred Ids</button>
+      </form>
     </div>
   );
 }
